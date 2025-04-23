517ccd3db7a6fbb0031ba40e876de2bb2f5a7df4
Jawad Jabbar <jawadjabbar90@gmail.com>
2025-04-23 15:38:22 +0500

    modified BST

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -1,71 +1,74 @@
 class Node {
-    constructor(value) {
-      this.value = value;
-      this.left = null;
-      this.right = null;
-    }
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
-  
-  class BinarySearchTree {
-    constructor() {
-      this.root = null;
-    }
-  
-    insert(value) {
-      const newNode = new Node(value);
-      if (!this.root) {
-        this.root = newNode;
-        return;
-      }
-  
-      let current = this.root;
-      while (true) {
-        if (value < current.value) {
-          if (!current.left) {
-            current.left = newNode;
-            return;
-          }
-          current = current.left;
-        } else {
-          if (!current.right) {
-            current.right = newNode;
-            return;
-          }
-          current = current.right;
-        }
-      }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return;
     }
 
-    search(value) {
-      let current = this.root;
-      while (current) {
-        if (value === current.value) return true;
-        value < current.value ? current = current.left : current = current.right;
-      }
-      return false;
-    }
-  
-    inOrderTraversal(node = this.root) {
-      if (node) {
-        this.inOrderTraversal(node.left);
-        console.log(node.value);
-        this.inOrderTraversal(node.right);
+    let current = this.root;
+    while (true) {
+      if (value < current.value) {
+        if (!current.left) {
+          current.left = newNode;
+          return;
+        }
+        current = current.left;
+      } else {
+        if (!current.right) {
+          current.right = newNode;
+          return;
+        }
+        current = current.right;
       }
     }
   }
-  
-  const bst = new BinarySearchTree();
-  bst.insert(50);
-  bst.insert(30);
-  bst.insert(70);
-  bst.insert(20);
-  bst.insert(40);
-  bst.insert(60);
-  bst.insert(80);
-  
-  console.log("Search 40:", bst.search(40)); 
-  console.log("Search 90:", bst.search(90)); 
-  
-  console.log("In-order Traversal:");
-  bst.inOrderTraversal();
-  
+
+  search(value) {
+    let current = this.root;
+    while (current) {
+      if (value === current.value) return true;
+      if (value < current.value) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+    return false;
+  }
+
+  inOrderTraversal(node = this.root) {
+    if (node) {
+      this.inOrderTraversal(node.left);
+      console.log(node.value);
+      this.inOrderTraversal(node.right);
+    }
+  }
+}
+
+const bst = new BinarySearchTree();
+bst.insert(50);
+bst.insert(30);
+bst.insert(70);
+bst.insert(20);
+bst.insert(40);
+bst.insert(60);
+bst.insert(80);
+
+console.log("Search 40:", bst.search(40));
+console.log("Search 90:", bst.search(90));
+
+console.log("In-order Traversal:");
+bst.inOrderTraversal();
